217017f152dd4095fefee6a4f20e0aab22f14aff
Fábio D. Batista <fabio@elementarsistemas.com.br>
2016-09-05 15:12:41 -0300

    Exemplo inicial em PowerShell

---
+++ powershell/sync.ps1
@@ -0,0 +1,64 @@
+###############################################################################
+# Script para sincronização de XMLs com o Portal Consyst-e
+# Documentação da integração: https://portal.consyste.com.br/doc/api
+###############################################################################
+
+param (
+   [Parameter(Mandatory=$true)][string]$AuthToken,   # o token de autenticação no Consyst-e
+   [string]$OutDir = '/tmp/xmls',                    # o diretório onde os XMLs serão salvos
+   [string]$Query = ''                               # consulta a rodar
+)
+
+$ErrorActionPreference = 'Stop'
+$baseUri = 'https://portal.consyste.com.br/api/v1'
+
+New-Item -ItemType Directory -Force -Path $OutDir | Out-Null
+
+function Call-Consyste {
+  $uri = "$baseUri$args"
+  Invoke-RestMethod -Headers @{ 'X-Consyste-Auth-Token' = $AuthToken } -Uri $uri
+}
+
+$res = Call-Consyste "/nfe/lista/recebidos?q=$([uri]::EscapeDataString($Query))"
+
+$sw1 = New-Object Diagnostics.Stopwatch
+$sw1.Start()
+
+$chaves = New-Object System.Collections.Generic.List[System.Object]
+while ($true) {
+  $novos = $res | foreach {$_.documentos.chave}
+  If ($novos.Count -eq 0) { break }
+  $chaves.AddRange($novos)
+  $pp = $res.proxima_pagina
+  Write-Progress "Coletando chaves" -PercentComplete ($chaves.Count / $res.total * 100)
+  $res = Call-Consyste "/nfe/lista/continua/$pp"
+}
+Write-Progress "Coletando chaves" -Completed
+$sw1.Stop()
+
+Write-Host
+Write-Host
+Write-Host
+Write-Host
+Write-Host
+Write-Host "Obtidas $($chaves.Count) chaves em $($sw1.Elapsed)"
+
+$sw2 = New-Object Diagnostics.Stopwatch
+$sw2.Start()
+$baixados = 0
+foreach ($chave in $chaves) {
+  Write-Host -NoNewLine "$chave "
+  $existe = Test-Path "$OutDir/$chave.xml"
+  If ($existe) {
+    Write-Host -ForegroundColor Gray "encontrado"
+  }
+  Else {
+    Write-Host -NoNewLine "baixando... "
+    Call-Consyste "/nfe/$chave/download.xml" | Out-File "$OutDir/$chave.xml"
+    Write-Host -ForegroundColor Green "OK!"
+    $baixados += 1
+  }
+}
+$sw2.Stop()
+
+Write-Host "Realizado o download de ${baixados} XMLs, em $($sw2.Elapsed)"
